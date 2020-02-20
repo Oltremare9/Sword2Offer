8d4992340c08a667d7a9472c6625412060996126
oltreamre9 <1134625169@qq.com>
2020-02-20 21:07:23 +0800

    面试题14_I.剪绳子;————贪婪 动规

---
+++ src/面试题14_I/剪绳子/Solution.java
@@ -0,0 +1,22 @@
+package 面试题14_I.剪绳子;
+
+class Solution {
+    //贪婪分为多个3
+    public int cuttingRope(int n) {
+        if (n == 2)
+            return 1;
+        if (n == 3)
+            return 2;
+        if (n == 4)
+            return 4;
+        int res = 3;
+        n -= 3;
+        while (n > 4) {
+            res *= 3;
+            res -= 3;
+        }
+        res *= n;
+        return res;
+    }
+
+}
